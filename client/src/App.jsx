2e63c221eca0b3be0b0aa32819673e820076ccaf
Mayank-decode <m4mayankpandit@gmail.com>
2026-08-15 18:58:40 +0530

    fix api url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-const API_URL = 'https://bmi-calculator-api-9jcn.onrender.com/api/bmi';
+const API_URL = '/api/bmi';
 
 function App() {
   const [form, setForm] = useState({ name: '', height: '', weight: '' });
